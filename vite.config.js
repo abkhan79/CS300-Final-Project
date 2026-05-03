f70d4d617c04c840b7132b5a6491459160035621
Aliya Khan <abkhan79@neiu.edu>
2026-05-03 17:24:24 -0500

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cs300-final-project/',
+  base: '/cs300-react-test/',
   plugins: [react()],
 })
