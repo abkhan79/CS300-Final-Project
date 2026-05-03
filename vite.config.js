a2aec5ea60b8c20b9f93eaa534e8188e5343eca8
Aliya Khan <abkhan79@neiu.edu>
2026-05-03 17:48:28 -0500

    chore: set GitHub Pages base path to repo name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cs300-final-project/',
+  base: '/CS300-Final-Project/',
   plugins: [react()],
 })
